b7380eddbcf76f4eba937150904d6a59bc716c67
ZeroOneKim <wowwow0124@naver.com>
2024-07-30 21:10:07 +0900

    Runner 생성

---
+++ src/main/java/binarykim/lifeutils/workManage/security/CumtomizedRunner.java
@@ -0,0 +1,21 @@
+package binarykim.lifeutils.workManage.security;
+
+import binarykim.lifeutils.workManage.WorkManager;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CumtomizedRunner implements ApplicationRunner {
+    private final WorkManager workManager;
+
+    public CumtomizedRunner(WorkManager workManager) {
+        this.workManager = workManager;
+    }
+
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        workManager.doManage();
+    }
+}
