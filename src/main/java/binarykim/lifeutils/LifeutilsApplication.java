641283f9e91f6ea71738b534bec96a998d416e35
ZeroOneKim <wowwow0124@naver.com>
2024-07-31 20:52:17 +0900

    Change new Method

--- src/main/java/binarykim/lifeutils/LifeutilsApplication.java
+++ src/main/java/binarykim/lifeutils/LifeutilsApplication.java
@@ -1,20 +1,25 @@
 package binarykim.lifeutils;
 
 import binarykim.lifeutils.workManage.WorkManager;
+import binarykim.lifeutils.workManage.security.CumtomizedRunner;
+import binarykim.lifeutils.workManage.timer.WorkDay;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class LifeutilsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LifeutilsApplication.class, args);
 	}
+
 	@Bean
-	public CommandLineRunner commandLineRunner(WorkManager workManager) {
-		return args -> {
-			workManager.doManage();
-		};
+	public ApplicationRunner applicationRunner(WorkManager workManager) {
+		return new CumtomizedRunner(workManager);
 	}
 }
