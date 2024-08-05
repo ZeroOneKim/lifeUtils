c67fc7e428a20a6e40b48a2369ea6c66d09bf292
ZeroOneKim <wowwow0124@naver.com>
2024-08-05 20:15:03 +0900

    Testing...

--- src/main/java/binarykim/lifeutils/workManage/WorkManager.java
+++ src/main/java/binarykim/lifeutils/workManage/WorkManager.java
@@ -3,6 +3,7 @@ package binarykim.lifeutils.workManage;
 import binarykim.lifeutils.workManage.timer.SchedulerTasks;
 import binarykim.lifeutils.workManage.timer.WorkDay;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,19 +12,33 @@ public class WorkManager {
     @Autowired WorkDay workDay;
     @Autowired SchedulerTasks schedulerTasks;
 
+    static int testCount = 0;
+
+    @Scheduled(cron = "0 53 8 * * ?")
     public void doManage() {
         if(workDay.getWorkDayList().isEmpty()) workDay.startInit();
-        Thread thread = new Thread();
-        workDay.getWorkDayList().contains(schedulerTasks.getNow()); //TODO it is Valid Logic..
 
-        try {
+        testCount++; //TODO DELETE it
+        if(testCount == 1) return;
+        if(workDay.getWorkDayList().contains(schedulerTasks.getNow())) {
+            try {
+                int sleepT = schedulerTasks.occurRandTime();
+                System.out.println("Wating ....   " + sleepT + " Seconds");
+                Thread.sleep(sleepT*1000);
+
+                schedulerTasks.getTestTime();
+                System.out.println(testCount + "] 작업 진행 됨.");
 //            pageModuler.loginPRC(); //Login
 //            thread.sleep(5000);
 //            pageModuler.isSKIP();   //popup Skip
 //            thread.sleep(500);
 
-        } catch (Exception e) {
-            e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            schedulerTasks.getTestTime();
+            System.out.println(testCount + "] 공휴일 및 휴일 Tasks..");
         }
     }
 }
