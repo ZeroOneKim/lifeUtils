86e2c462197c3b5782bc8b975ffe795b54f86f23
ZeroOneKim <wowwow0124@naver.com>
2024-08-20 21:23:36 +0900

    set IP

--- src/main/java/binarykim/lifeutils/workManage/WebDriverConfig.java
+++ src/main/java/binarykim/lifeutils/workManage/WebDriverConfig.java
@@ -1,10 +1,11 @@
 package binarykim.lifeutils.workManage;
 
+import binarykim.lifeutils.workManage.security.SecurityInfo;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.springframework.stereotype.Component;
 
 /**
  * @Configuration 사용 시, Spring Container가 종료 되는 시점 Bean을 정리 함.
@@ -17,10 +18,19 @@ public class WebDriverConfig {
     }
 
     public static WebDriver getDriver() {
+        String compIpInfo = new SecurityInfo().getCompIp();
+
         if (driver == null) {
             WebDriverManager.chromedriver().setup();
+
+            Proxy proxy = new Proxy();
+            proxy.setHttpProxy(compIpInfo);
+            proxy.setSslProxy(compIpInfo);
+
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--headless");
+            options.setProxy(proxy);
+
             driver = new ChromeDriver(options);
         }
         return driver;
