e2d56773b6ed1e6615cf8e260d2c54b5de509bcc
ZeroOneKim <wowwow0124@naver.com>
2024-08-20 21:25:35 +0900

    R F

--- src/main/java/binarykim/lifeutils/LifeutilsApplication.java
+++ src/main/java/binarykim/lifeutils/LifeutilsApplication.java
@@ -1,13 +1,8 @@
 package binarykim.lifeutils;
 
-import binarykim.lifeutils.workManage.WorkManager;
-import binarykim.lifeutils.workManage.CumtomizedRunner;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 @EnableScheduling
